feb33accacbe531eb3859ab79d2f8c54cfacb787
FORTECH\andrei.oltean <mihai.oltean2563@gmail.com>
2017-03-31 10:21:58 +0300

    Modal

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -9,11 +9,10 @@ var hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function(){
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssImport, mixins, cssvars, nested,hexrgba, autoprefixer]))
   .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
   .on('error', function(errorInfo){
     console.log(errorInfo.toString());
     this.emit('end');
   })
-  .pipe(gulp.dest('./app/temp/styles'))
+  .pipe(gulp.dest('./app/temp/styles'));
 });
